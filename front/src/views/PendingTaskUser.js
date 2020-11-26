2c5e4b9500b8f735446e8694107825b00d80542f
nf-alarcon@uol.com.br <nf-alarcon@uol.com.br>
2020-11-26 02:16:15 -0300

    resolvendo as merda

--- front/src/views/PendingTaskUser.js
+++ front/src/views/PendingTaskUser.js
@@ -1,29 +1,6 @@
-/*!
-
-=========================================================
-* Paper Dashboard React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-
-* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-/*eslint-disable*/
 import TaskUsers from "components/APICall/user/TaskUsers.js";
 import React from "react";
-// react plugin for creating notifications over the dashboard
-// reactstrap components
 import {
-  UncontrolledAlert,
-  Button,
   Card,
   CardHeader,
   CardBody,
