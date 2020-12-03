2fbe751fe9207ea5ff5d060ec8c36d8193072341
freitassantos <rodrigo.santos203@fatec.sp.gov.br>
2020-12-02 22:37:03 -0300

    ajustado a tela Tarefa

--- api/routes/funcionalidadesRoute.js
+++ api/routes/funcionalidadesRoute.js
@@ -5,7 +5,7 @@ const router = Router()
 
 router.get('/funcionalidades/projeto/:id', FuncionalidadeController.listaFuncionalidades)
 router.get('/funcionalidades/projeto/:id/status/:status_id', FuncionalidadeController.listaFuncionalidadesStatus)
-router.get('/funcionalidades/projeto/:id/task/:funcionalidade_id', FuncionalidadeController.pegaUmaFuncionalidade)
+router.get('/funcionalidades/:id', FuncionalidadeController.pegaUmaFuncionalidade)
 router.post('/funcionalidades', FuncionalidadeController.criaFuncionalidade)
 router.put('/funcionalidades/:id', FuncionalidadeController.AtualizaFuncionalidade)
 router.delete('/funcionalidades/:id', FuncionalidadeController.deletaUmaFuncionalidade)
